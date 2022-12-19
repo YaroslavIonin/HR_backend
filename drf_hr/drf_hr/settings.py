84afd4c4bcb5863a3a63a7a3ca1e6d6b7dd6142c
YaroslavIonin <ioninyaroslav22@gmail.com>
2022-12-19 13:22:40 +0500

    Fixed settings

--- drf_hr/drf_hr/settings.py
+++ drf_hr/drf_hr/settings.py
@@ -21,7 +21,7 @@ DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'hr.portal.very.interesting@gmail.com'
-EMAIL_HOST_PASSWORD = 'sbjcdsacacewyuwe'
+# EMAIL_HOST_PASSWORD = узнавать у владельца репозитория
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
