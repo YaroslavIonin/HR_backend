1eac77dd00a5b3a8798b25e2a53e72295a1c6c43
YaroslavIonin <ioninyaroslav22@gmail.com>
2023-04-08 22:28:57 +0500

    Update filters of Vacancy, Resume

--- drf_hr/back/filters.py
+++ drf_hr/back/filters.py
@@ -1,22 +1,34 @@
-from django_filters import rest_framework as filters
+from django_filters import rest_framework as filters, NumberFilter, ModelMultipleChoiceFilter
 
-from back.models import Vacancy, Resume
+from back.models import Vacancy, Resume, Skills
 
 
 class VacancyFilter(filters.FilterSet):
+    exp_work__gt = NumberFilter(field_name='exp_work', lookup_expr='gte')
+    exp_work__lt = NumberFilter(field_name='exp_work', lookup_expr='lte')
+
+    salary__gt = NumberFilter(field_name='salary', lookup_expr='gte')
+    salary__lt = NumberFilter(field_name='salary', lookup_expr='lte')
+
+    skills = ModelMultipleChoiceFilter(
+        field_name='skills',
+        queryset=Skills.objects.all(),
+    )
+
     class Meta:
         model = Vacancy
         fields = {
-            'exp_work': ['gte'],
-            'salary': ['gte'],
             'department': ['exact'],
         }
 
 
 class ResumeFilter(filters.FilterSet):
+    exp_work__gt = NumberFilter(field_name='exp_work', lookup_expr='gte')
+    exp_work__lt = NumberFilter(field_name='exp_work', lookup_expr='lte')
+    salary__gt = NumberFilter(field_name='salary', lookup_expr='gte')
+    salary__lt = NumberFilter(field_name='salary', lookup_expr='lte')
+
     class Meta:
         model = Resume
-        fields = {
-            'exp_work': ['gte'],
-            'salary': ['gte'],
-        }
+        fields = {}
+
